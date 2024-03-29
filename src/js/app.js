4b029627f86ded3a3f44cceee49a1cc2c322f8ef
CJLynch01 <cjlynch@byui.edu>
2024-03-28 19:28:31 -0600

    Save

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,52 @@
 import { getDate,getSunday,getMonday,getTuesday,getWednesday,getThursday,getFriday,getSaturday } from "./date.mjs";
-    // Logic to render the view based on the path
+import { fetchprimarylift, fetchaccessorylift } from "./utils.mjs";
+
+
+function addPLift() {
+
+    let list1 = [];
+    let list2 = [];
+    let list3 = [];
+    let list4 = [];
+    
+    let n = 1;
+    let x = 0;
+
+    let addRow = document.getElementById("tbl");
+    let newRow = addRow.insertRow(n);
+
+    list1[x] = getDate();
+    list2[x] = document.getElementById("primarylifts").value;
+    list3[x] = document.getElementById("plset").value;
+    list4[x] = document.getElementById("plrep").value;
+
+    let cel1 = newRow.insertCell(0);
+    let cel2 = newRow.insertCell(1);
+    let cel3 = newRow.insertCell(2);
+    let cel4 = newRow.insertCell(3);
+    let cel5 = newRow.insertCell(4);
+
+    cel1.innerHTML = list1[x];
+    cel2.innerHTML = list2[x];
+    cel3.innerHTML = list3[x];
+    cel4.innerHTML = list4[x];
+    cel5.innerHTML = ' <input class="removePRow" type="button" onclick="deletePLift(this)" value="Delete"></input>';
+    cel5.querySelector('.removePRow').addEventListener('click', function() {
+        deletePLift(this);
+    });
+    n++;
+    x++;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchprimarylift(primarylifts);
+    fetchaccessorylift(accessorylifts);
+    addALift();
+    addPLift();
+    deleteALift();
+    deletePLift();
+});
+
 
 
 window.addEventListener('popstate', handleRouteChange);
@@ -13,7 +60,6 @@ function homeView() {
 
 
 function dayWorkout() {
-    // currentDate = getDate()
     return `<h1>Today's Workout</h1>
     <div id="current-date">${getDate()}</div>
 
@@ -21,25 +67,24 @@ function dayWorkout() {
         <fieldset>
         <legend>Primary Lifts</legend>
         <select name="primarylifts" id="primarylifts"></select>
-            Sets:<input id="pset" type="text" name="plsets">
-            Reps:<input id="prep" type="text" name="plreps">
+            Sets:<input id="plset" type="text" name="plsets">
+            Reps:<input id="plrep" type="text" name="plreps">
+            <input class="addlift" id="addlift" type="button" onclick="addPLift" value="Add Data" />
         </fieldset>
 
         <fieldset>
         <legend>Accessory Lifts</legend>
         <select name="accessorylifts" id="accessorylifts"></select>
-            Sets:<input type="text" name="alsets">
-            Reps:<input type="text" name="alreps">
-        </fieldset>
-        
-        <input type="button" name="add" value="Add Data" onclick="addLift();" class="btn btn-success">
-    
+            Sets:<input id="alset" type="text" name="alsets">
+            Reps:<input id="alrep" type="text" name="alreps">
+            <input id="addaccessory" type="button" onclick="addALift()" value="Add Data" />
+        </fieldset>    
     </form>
 
     <table id="tbl" class="table" border="1">
         <thead>
             <th>Date</th> 
-            <th>Lift</th>
+            <th>Primary Lift</th>
             <th>Sets</th>
             <th>Reps</th>
         </thead>
@@ -47,6 +92,18 @@ function dayWorkout() {
          
         </tbody>
     </table>
+
+    <table id="tbl2" class="table" border="1">
+    <thead>
+        <th>Date</th> 
+        <th>Accessory Lift</th>
+        <th>Sets</th>
+        <th>Reps</th>
+    </thead>
+    <tbody>
+     
+    </tbody>
+</table>
 
     
     <button type="button" id="workoutToday">Submit</button>
@@ -70,7 +127,16 @@ function weekWorkout() {
 }
 
 function journal() {
-    return `<h1>Journal</h1>`;
+    return `<h1>Journal</h1>
+    
+    <h2>Create New Entry</h2>
+
+    <div id="jdate">${getDate()}</div>
+    Title: <input type="text" id="jinput" />
+    Entry: <input type="text" id="jentry" />
+
+
+    `;
 }
 
 // Update handleRouteChange to render views
@@ -96,6 +162,7 @@ function handleRouteChange() {
     }
 
     document.getElementById('app').innerHTML = view;
+
 }
 
 document.querySelectorAll('.route').forEach(link => {
@@ -105,3 +172,104 @@ document.querySelectorAll('.route').forEach(link => {
         handleRouteChange();
     });
 });
+
+
+
+// function addPLift() {
+
+//     let list1 = [];
+//     let list2 = [];
+//     let list3 = [];
+//     let list4 = [];
+    
+//     let n = 1;
+//     let x = 0;
+
+//     let addRow = document.getElementById("tbl");
+//     let newRow = addRow.insertRow(n);
+
+//     list1[x] = getDate();
+//     list2[x] = document.getElementById("primarylifts").value;
+//     list3[x] = document.getElementById("plset").value;
+//     list4[x] = document.getElementById("plrep").value;
+
+//     let cel1 = newRow.insertCell(0);
+//     let cel2 = newRow.insertCell(1);
+//     let cel3 = newRow.insertCell(2);
+//     let cel4 = newRow.insertCell(3);
+//     let cel5 = newRow.insertCell(4);
+
+//     cel1.innerHTML = list1[x];
+//     cel2.innerHTML = list2[x];
+//     cel3.innerHTML = list3[x];
+//     cel4.innerHTML = list4[x];
+//     cel5.innerHTML = ' <input class="removePRow" type="button" onclick="deletePLift(this)" value="Delete"></input>';
+//     cel5.querySelector('.removePRow').addEventListener('click', function() {
+//         deletePLift(this);
+//     });
+//     n++;
+//     x++;
+// }
+
+function deletePLift(button) {
+    var row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+function addALift() {
+
+    let list1 = [];
+    let list2 = [];
+    let list3 = [];
+    let list4 = [];
+    
+    let n = 1;
+    let x = 0;
+
+    let addRow = document.getElementById("tbl2");
+    let newRow = addRow.insertRow(n);
+
+    list1[x] = getDate();
+    list2[x] = document.getElementById("accessorylifts").value;
+    list3[x] = document.getElementById("alset").value;
+    list4[x] = document.getElementById("alrep").value;
+
+    let cel1 = newRow.insertCell(0);
+    let cel2 = newRow.insertCell(1);
+    let cel3 = newRow.insertCell(2);
+    let cel4 = newRow.insertCell(3);
+    let cel5 = newRow.insertCell(4);
+
+    cel1.innerHTML = list1[x];
+    cel2.innerHTML = list2[x];
+    cel3.innerHTML = list3[x];
+    cel4.innerHTML = list4[x];
+    cel5.innerHTML = ' <input class="removeARow" type="button" onclick="deleteALift(this)" value="Delete"></input>';
+    cel5.querySelector('.removeARow').addEventListener('click', function() {
+        deleteALift(this);
+    });
+
+    n++;
+    x++;
+    
+}
+
+function deleteALift(button) {
+    var row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+// document.addEventListener('click', function (e) {
+//     if(hasClass(e.target, 'addlift')) {
+
+//     }
+// })
+
+
+document.getElementById('addlift').addEventListener('click', addPLift); // Add primary lift data to the table on click
+document.getElementById('addaccessory').addEventListener('click', addALift); // Add accessory lift data to the table on click
+
+// Array.from(document.getElementsByClassName("addlift"))
+//     .forEach(function(element){
+//         element.addEventListener("click", addPLift);
+//     });
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,77 +1,44 @@
 const url1 = "../json/primary.json"
 const url2 = "../json/accessory.json"
-const primarylifts = document.getElementById("primarylifts")
-const secondarylifts = document.getElementById("accessorylifts")
 
-
-//fetch primary lifts from JSON file
-async function fetchprimarylift() {
+export async function fetchprimarylift(primarylifts) {
     await fetch(url1)
     .then(res => res.json())
-    .then(data => loadprimarylift(data))
+    .then(data => loadprimarylift(data, primarylifts))
     .catch(err => console.log(err))
 }
+
 //load primary lifts from JSON file
-function loadprimarylift(data) {
-    for(var key in data){
-    var list=data[key]
-        for(var obj in list)
-        {
+export function loadprimarylift(data, primarylifts) {
+    console.log("data received:", data);
+    for(var key in data) {
+        var list=data[key];
+        for(var obj in list) {
             var opt = document.createElement("option");
             opt.text = list[obj];
             opt.value = list[obj];
-
             primarylifts.add(opt);
         }
     }
 }
 
 //fetch accessory lifts from JSON file
-async function fetchaccessorylift() {
+export async function fetchaccessorylift(secondarylifts) {
     await fetch(url2)
     .then(res => res.json())
-    .then(data => loadaccessorylift(data))
+    .then(data => loadaccessorylift(data, secondarylifts))
     .catch(err => console.log(err))
 }
+
 //load accessory lifts from JSON file
-function loadaccessorylift(data) {
+export function loadaccessorylift(data, secondarylifts) {
     for(var key in data){
-    var list=data[key]
-        for(var obj in list)
-        {
+        var list=data[key]
+        for(var obj in list) {
             var opt = document.createElement("option");
             opt.text = list[obj];
             opt.value = list[obj];
-
             secondarylifts.add(opt);
         }
     }
 }
-
-function addLift() {
-    var pexercise = document.getElementById("primarylifts").value;
-    var pset = document.getElementById("pset").value;
-    var prep = document.getElementById("prep").value;
-    var date = document.getElementById("current-date").value;
-
-    var tr = document.createElement('tr');
-
-    var td1 = tr.appendChild(document.createElement('td'));
-    var td2 = tr.appendChild(document.createElement('td'));
-    var td3 = tr.appendChild(document.createElement('td'));
-    var td4 = tr.appendChild(document.createElement('td'));
-
-    td1.innerHTML=date;
-    td2.innerHTML=pexercise;
-    td3.innerHTML=pset;
-    td4.innerHTML=prep;
-
-    document.getElementById("tbl").appendChild(tr);
-
-}
-
-
-
-addLift()
-fetchprimarylift()
-fetchaccessorylift()
